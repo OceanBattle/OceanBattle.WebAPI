64310ded0429a808c8feb6d731580a69d2824125
kurowskitomek <tomekkurowski2003@gmail.com>
2022-11-08 21:38:15 +0100

    Add project files.

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
